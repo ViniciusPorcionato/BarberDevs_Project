262a9827b90b23dca74311b253dd5d041778b100
Thiago Domingues Rafael <47195701867@info.local>
2024-05-29 16:18:13 -0300

    navegação feita

--- barberdevs/App.js
+++ barberdevs/App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// NAVEGAÇÃO 
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+// TELAS
+import { TelaEntrada } from "./src/screens/TelaEntrada/TelaEntrada";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* TELA DE INICIO */}
+        <Stack.Screen
+          name="TelaEntrada"
+          component={TelaEntrada}
+          options={{ title: "TelaEntrada" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
