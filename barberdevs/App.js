2b4ecb72c6a974ee5ad85f7118306544071ff34e
Miguel Lamarca Moreira <50866670890@info.local>
2024-05-29 16:21:16 -0300

    att

--- barberdevs/App.js
+++ barberdevs/App.js
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
+import {Poppins_400Regular, Poppins_600SemiBold} from "@expo-google-fonts/poppins";
 
 export default function App() {
+
+  let [fontsLoaded, fontError] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
+
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
