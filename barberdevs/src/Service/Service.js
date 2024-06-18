f9441b7939345dafe53f705edc3a5188fae84999
Vinicius <porcionatovinicius@gmail.com>
2024-06-18 10:19:27 -0300

    .

--- barberdevs/src/Service/Service.js
+++ barberdevs/src/Service/Service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const portaAPI = '5279';
 
-const ipViniciusP = '192.168.21.113';
+const ipViniciusP = '192.168.21.137';
 
 const apiUrlLocal = `http://${ipViniciusP}:${portaAPI}/api`
 
